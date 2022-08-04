637bf11bb728a5582577654d9e92b82f81b45529
gaeldumon <dmnleag@protonmail.com>
2022-08-04 13:37:07 +0200

    Final version of upload form is up. Front only. Mockup upload action url (mocky) (API is not up yet).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { UploadFormComponentComponent } from './upload-form-component/upload-form-component.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { fr_FR } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
+import {AppComponent} from './app.component';
+import {UploadFormComponentComponent} from './upload-form-component/upload-form-component.component';
+import {NZ_I18N} from 'ng-zorro-antd/i18n';
+import {fr_FR} from 'ng-zorro-antd/i18n';
+import {registerLocaleData} from '@angular/common';
 import fr from '@angular/common/locales/fr';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NzTypographyModule} from "ng-zorro-antd/typography";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzUploadModule} from "ng-zorro-antd/upload";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzTagModule} from "ng-zorro-antd/tag";
+import {NzNoAnimationModule} from "ng-zorro-antd/core/no-animation";
+import {NzMessageService} from "ng-zorro-antd/message";
+
+import {IconDefinition} from '@ant-design/icons-angular';
+
+import {DeleteOutline, DownloadOutline, InboxOutline} from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [DeleteOutline, DownloadOutline, InboxOutline];
 
 registerLocaleData(fr);
 
@@ -22,9 +38,20 @@ registerLocaleData(fr);
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzTypographyModule,
+    NzFormModule,
+    NzInputModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzUploadModule,
+    NzIconModule.forRoot(icons),
+    NzTagModule,
+    NzNoAnimationModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
+  providers: [{provide: NZ_I18N, useValue: fr_FR}, NzMessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/upload-form-component/upload-form-component.component.ts
+++ src/app/upload-form-component/upload-form-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {NzUploadChangeParam} from "ng-zorro-antd/upload";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-upload-form-component',
@@ -6,10 +8,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-form-component.component.css']
 })
 export class UploadFormComponentComponent implements OnInit {
+  public tagInputVisible: boolean;
+  public tagInputValue: string;
+  public tags: string[];
+  public docUploadActionUrl: string;
+  @ViewChild('inputElement', {static: false}) inputElement?: ElementRef;
 
-  constructor() { }
+  constructor(private docUploadMessage: NzMessageService) {
+    this.tagInputVisible = false;
+    this.tagInputValue = "";
+    this.tags = [];
+    this.docUploadActionUrl = "https://www.mocky.io/v2/5cc8019d300000980a055e76"
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   *
+   */
+  submitForm(): void {
+    console.log("Form submitted");
+  }
+
+  /**
+   *
+   * @param file
+   * @param fileList
+   */
+  handleChange({file, fileList}: NzUploadChangeParam): void {
+    const status = file.status;
+    if (status !== "uploading") {
+      console.log(file, fileList);
+    }
+    if (status === "done") {
+      this.docUploadMessage.success(`${file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      this.docUploadMessage.error(`${file.name} file upload failed.`);
+    }
+  }
+
+  /**
+   * Filter the tags property on the tags which had not been removed.
+   * This method mutate the tags property directly.
+   * @param removedTag
+   */
+  handleClose(removedTag: {}): void {
+    this.tags = this.tags.filter(tag => tag !== removedTag);
+  }
+
+  /**
+   * Return the 20 first characters of tag if tag is > 20 characters.
+   * Return tag directly if tag <= 20 characters.
+   * @param tag
+   */
+  sliceTagName(tag: string): string {
+    return (tag.length > 20) ? `${tag.slice(0, 20)}...` : tag;
+  }
+
+  /**
+   *
+   */
+  showInput(): void {
+    this.tagInputVisible = true;
+    setTimeout(() => {
+      this.inputElement?.nativeElement.focus();
+    }, 10);
+  }
+
+  /**
+   *
+   */
+  handleInputConfirm(): void {
+    if (this.tagInputValue && this.tags.indexOf(this.tagInputValue) === -1) {
+      this.tags = [...this.tags, this.tagInputValue];
+    }
+    this.tagInputValue = "";
+    this.tagInputVisible = false;
+  }
 }
