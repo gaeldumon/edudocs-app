c999c07aa3b5e4fa597953226a4c427c0eed785e
gaeldumon <dmnleag@protonmail.com>
2022-08-04 09:07:14 +0200

    Init project. Created a form-upload component. Installed ngzorro.

---
+++ src/app/upload-form-component/upload-form-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-form-component',
+  templateUrl: './upload-form-component.component.html',
+  styleUrls: ['./upload-form-component.component.css']
+})
+export class UploadFormComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
